421d8472ca359f5372e1c7b1bcf790f9c38e2ba8
Franklin Zeron <1210129@usap.edu>
2023-06-23 14:07:01 -0600

    habilito servicio y conexion a la base de datos

--- apps.js
+++ apps.js
@@ -8,22 +8,23 @@ res.send("Bienvenido");
 
 app.listen(port, function () {
 console.log(`Puerto ${port}!`);
-});
+}); //hasta aqui el servidor
 
+//aqui comienza la conexion a base de datos
 var Connection = require('tedious').Connection;  
     var config = {  
-        server: 'VMI845837\SQLEXPRESS',  
+        server: 'DESKTOP-HG7D92R\SQLEXPRESS',  
         authentication: {
             type: 'default',
             options: {
-                userName: 'sa', 
-                password: 'Gestion123$'  
+                userName: 'Frank', 
+                password: 'Frank'  
             }
         },
         options: {
             
             encrypt: true,
-            database: 'academics'  
+            database: 'ventas'  
         }
     };  
     var connection = new Connection(config);  
